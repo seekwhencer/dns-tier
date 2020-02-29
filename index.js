53edc400a8e9b19adb855f67518be635fa6c76be
Matthias Kallenbach <matthias.kallenbach@gmail.com>
2020-02-29 14:06:51 +0100

    better start chaining, share internet

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ import App from './lib/App.js';
 const namespace = 'DNSTIER';
 
 // create the app and run it
-new App().then(app => global[namespace] = app);
+new App()
+    .then(app => global[namespace] = app)
+    .then(() => {
+        LOG('');
+        LOG('');
+        LOG('/////////');
+        LOG('/// >>> LÄUFT ...');
+        LOG('');
+    });
